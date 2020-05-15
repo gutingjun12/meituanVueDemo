e11f5cc842656b2404fcc3f23af1ca46e11ca239
gutingjun12 <714979470@qq.com>
2020-05-15 13:11:33 +0800

    ip修改

--- meituan1/vue.config.js
+++ meituan1/vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
 
 	devServer: {
-		host: '192.168.0.64',
+		host: 'localhost',
 		port: 8080, // 端口号
 		https: false, // https:{type:Boolean}
 		open: true, //配置自动启动浏览器
 		proxy: {
 			'/api': {
-				target: 'http://192.168.0.64:8080', //http://localhost:8080
+				target: 'http://localhost:8080', //http://localhost:8080
 				pathRewrite: {
 					'^/api': ''
 				},
